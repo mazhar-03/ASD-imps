703b8013097cc50b5d3b0e28a0c3b7fcfe3334b8
Mazhar Altincay <mazharaltincay89@gmail.com>
2024-11-02 00:57:24 +0100

    used getInts() like a senior.

--- src/skipping_algorithm/Main.java
+++ src/skipping_algorithm/Main.java
@@ -1,13 +1,10 @@
 package skipping_algorithm;
 
-import binary_search.BinarySearch;
-import merge_sort.MergeSort;
+import static binary_search.Main.getInts;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{64, 1,4, 7, 2,0, 65, 2, 5, 6,2, 2, 1, 56, 7, 4,9,0,2,5,8,4,23,12,3,16,16,12,1,5};
-        MergeSort.mergeSort(arr, 0, arr.length-1);
-
+        int[] arr = getInts();
         System.out.print("the index at: " + JumpSearch.jumpSearch(arr, arr.length, 5));
         System.out.println(" | Length: " + arr.length);
     }
